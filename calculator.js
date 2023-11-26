32cc2f1f90f98f3f7f425bf13360d57273229a81
Tony Lafortune <tonylafortune305@gmail.com>
2023-11-25 22:28:31 -0500

    css and java

---
+++ calculator.js
@@ -0,0 +1,18 @@
+
+
+function seeDisplay(value) {
+    document.getElementById('display').value += value ;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        var result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
